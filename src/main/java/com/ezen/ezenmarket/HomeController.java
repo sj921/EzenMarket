4a752f425783df0de040db049e76917afd22fd43
SungHoon Bae <baesunghoon81@gmail.com>
2023-02-13 12:14:00 +0900

    배성훈 테스트커밋

--- src/main/java/com/ezen/ezenmarket/HomeController.java
+++ src/main/java/com/ezen/ezenmarket/HomeController.java
@@ -32,8 +32,7 @@ public class HomeController {
 		String formattedDate = dateFormat.format(date);
 		
 		model.addAttribute("serverTime", formattedDate );
-		
+		System.out.println("드디어 됐다");
 		return "home";
 	}
-	
 }
