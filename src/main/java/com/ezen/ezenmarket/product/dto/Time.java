74b70173064220c008f9bc675d1870e7d93e0ec0
sj921 <111365047+sj921@users.noreply.github.com>
2023-03-03 15:57:38 +0900

    카테고리별 출력->최신순으로 수정

--- src/main/java/com/ezen/ezenmarket/product/dto/Time.java
+++ src/main/java/com/ezen/ezenmarket/product/dto/Time.java
@@ -17,7 +17,7 @@ public class Time {
        } else if (minutes < 60) {
            return String.format("%d분 전", minutes);
        } else if (minutes < 1440) {
-           return String.format("%d시 전", minutes / 60);
+           return String.format("%d시간 전", minutes / 60);
        } else {
            return pastTime.format(DateTimeFormatter.ofPattern("MMM dd, yyyy"));
        }
